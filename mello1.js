cc7e6574d524cea23f62bb375bf24ff1991f3830
Zeus30007 <130933523+Zeus30007@users.noreply.github.com>
2023-12-10 06:21:14 -0800

    Add files via upload

--- mello1.js
+++ mello1.js
@@ -173,9 +173,9 @@ try {
         ZeusBotInc.sendMessage(from, { sticker: ZeusStikRep }, { quoted: m })
         }
                    
-        //TIME
+      //TIME
         const xtime = moment.tz('Asia/Kolkata').format('HH:mm:ss')
-        const xdate = moment.tz('Asia/Kolkata'').format('DD/MM/YYYY')
+        const xdate = moment.tz('Asia/Kolkata').format('DD/MM/YYYY')
         const time2 = moment().tz('Asia/Kolkata').format('HH:mm:ss')  
          if(time2 < "23:59:00"){
 var Zeusytimewisher = `Good Night 🌌`
